25889f423ba3be7530ee74dd9e7a303ac96c69b1
irenap89 <irenap89@gmail.com>
2023-07-30 19:07:25 +0300

    first lesson

---
+++ src/Bg.js
@@ -0,0 +1,14 @@
+import './Bg.css';
+
+function Bg() {
+  return (
+    <div className="Bg">
+     <div className="header">
+        <span className='header_text'> העלאת תמונה כדי להסיר את הרקע </span>
+        <button className="header_btn"> העלאת תמונה</button>
+     </div>
+    </div>
+  );
+}
+
+export default Bg;
