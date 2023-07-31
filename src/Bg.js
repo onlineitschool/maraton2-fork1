0a1b8432b0d169636237526943b2f1a2765a215b
irenap89 <irenap89@gmail.com>
2023-07-31 19:02:28 +0300

    lesson 2

--- src/Bg.js
+++ src/Bg.js
@@ -6,7 +6,29 @@ function Bg() {
      <div className="header">
         <span className='header_text'> העלאת תמונה כדי להסיר את הרקע </span>
         <button className="header_btn"> העלאת תמונה</button>
+        <span className="header_subtext"> פורמטים נתמכים png, jpeg</span>
      </div>
+
+    <div className="main_div">
+
+      <div className="left_div"> LEFT </div>
+
+      <div className="right_div">
+        <div className="right_div_middle_div">
+
+          <div className="right_div_top">
+              <div className="right_div_top_text"> תמונה חינם </div>
+              <div className="right_div_top_subtext">  612X408 תצוגה מקדימה של תמונה </div>
+              <button className="right_div_top_btn"> הורד </button>
+              <div className="right_div_top_sub_sub_text"> איכות טובה עד 0.25 מגה פיקסל </div>
+          </div>
+          <div className="right_div_bottom"></div>
+
+        </div>
+      </div>
+
+    </div>
+
     </div>
   );
 }
